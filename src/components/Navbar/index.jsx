c8f42155c7d365f03c352e7906c085cc2800a003
Ianel <ianell.s118@gmail.com>
2023-11-15 13:46:46 +0300

    feat: add animation on navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,46 +14,49 @@ function Navbar() {
 
     return (
         <div className="relative">
-            <nav className=" relative z-10 flex justify-between items-center px-6 py-3 md:px-12 md:py-4 shadow-md shadow-indigo-300">
-                <h1 className="text-2xl font-semibold">
-                    I<span className="text-indigo-500">a</span>nel T
-                    <span className="text-indigo-500">o</span>mbozafy
+            <div className="sticky w-screen h-3 bg-indigo-500">
+                <div className="w-16 md:w-48 h-3 rounded-lg bg-black bg-clip-padding backdrop-blur-lg backdrop-filter opacity-50 animate-slideSmall md:animate-slideLarge"></div>
+            </div>
+
+            <nav className="sticky z-10 flex items-center px-6 py-3 md:px-12 md:py-6">
+                <h1 className="text-2xl font-semibold flex justify-start self-start">
+                    IT
                 </h1>
-                <div className="lg:flex hidden items-center gap-6">
+                <div className="lg:flex hidden absolute left-1/2 -translate-x-1/2 items-center gap-6">
                     <a
-                        className=" hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
+                        className="transform transition-all hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
                         href="#home"
                     >
                         Home
                     </a>
                     <a
-                        className=" hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
+                        className="transform transition-all hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
                         href="#skills"
                     >
                         Skills
                     </a>
                     <a
-                        className=" hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
+                        className="transform transition-all hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
                         href="#projects"
                     >
                         Projects
                     </a>
                     <a
-                        className=" hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
+                        className="transform transition-all hover:text-indigo-500 border-b-2 border-b-transparent hover:border-b-indigo-500"
                         href="#testimonials"
                     >
                         Testimonials
                     </a>
                 </div>
-                <a
+                {/*  <a
                     href="mailto:tombozafy.ianel@gmail.com"
                     className="lg:bg-indigo-500 hidden lg:block lg:hover:bg-indigo-800 lg:text-white lg:px-4 lg:py-2"
                 >
                     Contact Me
-                </a>
+                </a> */}
                 {showNav ? (
                     <button
-                        className="lg:hidden transition-all transform"
+                        className="lg:hidden absolute right-6 transition-all transform"
                         onClick={() => {
                             setShowNav(false);
                         }}
@@ -62,7 +65,7 @@ function Navbar() {
                     </button>
                 ) : (
                     <button
-                        className="lg:hidden transition-all transform"
+                        className="lg:hidden absolute right-6 transition-all transform"
                         onClick={() => {
                             setShowNav(true);
                         }}
@@ -72,7 +75,7 @@ function Navbar() {
                 )}
             </nav>
             {showNav && (
-                <div className="absolute top-14 md:top-16 overflow-y-hidden h-screen bg-indigo-500 left-0 z-50 flex flex-col justify-center items-center w-screen lg:hidden">
+                <div className="absolute top-16 md:top-20 overflow-hidden h-screen bg-indigo-500 left-0 z-50 flex flex-col justify-center items-center w-screen lg:hidden">
                     <a
                         onClick={() => setShowNav(false)}
                         className="py-3 hover:py-3 border-b-2 hover:w-full hover:text-center hover:bg-white hover:text-indigo-500 text-white border-b-transparent hover:border-b-white-500"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,46 @@ module.exports = {
             boxShadow: {
                 inv: "0 -10px 15px -3px rgb(67 56 202 / 0.1), 0 -4px 6px -4px rgb(67 56 202 / 0.1)",
             },
+            animation: {
+                slideLarge: "slideDivLarge 5s ease-in-out infinite alternate",
+                slideSmall: "slideDivSmall 5s ease-in-out infinite alternate",
+            },
+            keyframes: {
+                slideDivLarge: {
+                    "0%": {
+                        transform: "translateX(0%)",
+                    },
+                    "25%": {
+                        transform: "translateX(25vw)",
+                    },
+                    "50%": {
+                        transform: "translateX(50vw)",
+                    },
+                    "75%": {
+                        transform: "translateX(75vw)",
+                    },
+                    "100%": {
+                        transform: "translateX(calc(100vw - 12rem))",
+                    },
+                },
+                slideDivSmall: {
+                    "0%": {
+                        transform: "translateX(0%)",
+                    },
+                    "25%": {
+                        transform: "translateX(25vw)",
+                    },
+                    "50%": {
+                        transform: "translateX(50vw)",
+                    },
+                    "75%": {
+                        transform: "translateX(75vw)",
+                    },
+                    "100%": {
+                        transform: "translateX(calc(100vw - 4rem))",
+                    },
+                },
+            },
         },
     },
     plugins: [],
